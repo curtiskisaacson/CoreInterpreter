2d821f7f191195079899e787e78f487c7c5d17da
curtisisaacson <curtiskisaacson@gmail.com>
2019-09-30 18:11:14 -0400

    Updates test cases and nextWordOrSep
    Added tests cases for scan method and added functionality to
    nextWordOrSeparator to help check for important separators that are
    actually tokens in the core language like semicolon

--- src/code/CoreScanner.java
+++ src/code/CoreScanner.java
@@ -1,7 +1,9 @@
 package code;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,107 +13,124 @@ import java.util.Set;
 public class CoreScanner {
 	public File f;
 	public Scanner in;
-	
-    public CoreScanner (String fileName) throws FileNotFoundException{
-    	f = new File(fileName);
-    	in = new Scanner(f);
-    	
-    }
 
-    public String[] scan (){
-        ArrayList<String> tokens = new ArrayList<>();
-        Set<Character> separators = new HashSet<>();
+	public CoreScanner(String fileName) throws FileNotFoundException {
+		f = new File(fileName);
+		in = new Scanner(f);
 
-        separators.add(' ');
-        separators.add('/');
-        separators.add('\n');
-        separators.add('\t');
+	}
 
-        String nextLine;
-        while(in.hasNext()){
-            nextLine = in.nextLine();
-            int pos = 0;
-            String next = "";
+	public String[] scan() {
+		ArrayList<String> tokens = new ArrayList<>();
+		Set<Character> separators = new HashSet<>();
 
-            while(pos < nextLine.length()){
-                next = nextWordOrSeparator(nextLine,pos,separators);
-                pos+= next.length();
+		separators.add(' ');
+		separators.add('.');
+		separators.add('\n');
+		separators.add('\t');
+		separators.add('\'');
+		
+		
+		Set<Character> importantChars = new  HashSet<>();
+		
+		importantChars.add(':');
+		importantChars.add(';');
+		importantChars.add(',');
+		importantChars.add('=');
+		importantChars.add('!');
+		importantChars.add('+');
+		importantChars.add('*');
+		importantChars.add('-');
+		importantChars.add('/');
 
-                if(separators.contains(next.charAt(0))){
-                    tokens.add(next);
-                }
-            }
+		String nextLine;
+		while (in.hasNext()) {
+			nextLine = in.nextLine();
+			int pos = 0;
+			String next = "";
 
-        }
-        
-        return (String[])tokens.toArray();
-    }
-    
-    public String[] preprocess(String[] tokens) {
-    	Map<String,String> tokenMap = new HashMap<String,String>();
-    	tokenMap.put("program", "PROGRAM");
-    	tokenMap.put("begin", "BEGIN");
-    	tokenMap.put("end", "END");
-    	tokenMap.put("else", "ELSE");
-    	
-    	tokenMap.put("int", "INT");
-    	
-    	tokenMap.put("if", "IF");
-    	tokenMap.put("then","THEN");
-    	tokenMap.put("else", "ELSE");
-    	tokenMap.put("endif", "ENDIF");
-    	
-    	tokenMap.put("input", "INPUT");
-    	tokenMap.put("output", "OUTPUT");
-    	
-    	tokenMap.put("while", "WHILE");
-    	tokenMap.put("endwhile", "ENDWHILE");
-    	
-    	tokenMap.put(";", "SEMICOLON");
-    	
-    	
-    	
-    	
-    	ArrayList<String> preprocessedTokens = new ArrayList<>();
-    	return (String[])preprocessedTokens.toArray();
-    }
-    
-    
+			while (pos < nextLine.length()) {
+				next = nextWordOrSeparatorOrImportant(nextLine, pos, separators,importantChars);
+				pos += next.length();
 
-    private static String nextWordOrSeparator(String text, int position,
-                                              Set<Character> separators) {
-        assert text != null : "Violation of: text is not null";
-        assert separators != null : "Violation of: separators is not null";
-        assert 0 <= position : "Violation of: 0 <= position";
-        assert position < text.length() : "Violation of: position < |text|";
+				if (!separators.contains(next.charAt(0))) {
+					tokens.add(next);
+				}
+			}
 
-        int count = 0;
-        char returnedPiece = 'a';
-        String returned = "";
-        if (separators.contains(text.charAt(position))) {
-            while (count < text.substring(position, text.length()).length()) {
-                returnedPiece = text.charAt(position + count);
-                if (separators.contains(text.charAt(position + count))) {
-                    returned = returned + returnedPiece;
-                    count++;
-                } else {
-                    count = text.substring(position, text.length()).length();
-                }
-            }
-            count = 0;
-        } else {
-            while (count < text.substring(position, text.length()).length()) {
-                returnedPiece = text.charAt(position + count);
-                if (!separators.contains(text.charAt(position + count))) {
-                    returned = returned + returnedPiece;
-                    count++;
-                } else {
-                    count = text.substring(position, text.length()).length();
-                }
-            }
-            count = 0;
-        }
-        return returned;
-    }
+		}
+
+		Object[] tokensObArr = tokens.toArray();
+
+		// convert Object array to String array
+		String[] tokensArray = Arrays.copyOf(tokensObArr, tokensObArr.length, String[].class);
+
+		return tokensArray;
+	}
+
+	public String[] preprocess(String[] tokens) {
+		Map<String, String> tokenMap = new HashMap<String, String>();
+		tokenMap.put("program", "PROGRAM");
+		tokenMap.put("begin", "BEGIN");
+		tokenMap.put("end", "END");
+		tokenMap.put("else", "ELSE");
+
+		tokenMap.put("int", "INT");
+
+		tokenMap.put("if", "IF");
+		tokenMap.put("then", "THEN");
+		tokenMap.put("else", "ELSE");
+		tokenMap.put("endif", "ENDIF");
+
+		tokenMap.put("input", "INPUT");
+		tokenMap.put("output", "OUTPUT");
+
+		tokenMap.put("while", "WHILE");
+		tokenMap.put("endwhile", "ENDWHILE");
+
+		tokenMap.put(";", "SEMICOLON");
+
+		ArrayList<String> preprocessedTokens = new ArrayList<>();
+
+		return (String[]) preprocessedTokens.toArray();
+	}
+
+	private static String nextWordOrSeparatorOrImportant(String text, int position, Set<Character> separators,
+			Set<Character> importantChars) {
+		assert text != null : "Violation of: text is not null";
+		assert separators != null : "Violation of: separators is not null";
+		assert 0 <= position : "Violation of: 0 <= position";
+		assert position < text.length() : "Violation of: position < |text|";
+
+		int count = 0;
+		char returnedPiece = 'a';
+		String returned = "";
+		if (separators.contains(text.charAt(position))) {
+			while (count < text.substring(position, text.length()).length()) {
+				returnedPiece = text.charAt(position + count);
+				if (separators.contains(text.charAt(position + count))) {
+					returned = returned + returnedPiece;
+					count++;
+				} else {
+					count = text.substring(position, text.length()).length();
+				}
+			}
+			count = 0;
+		} else if (importantChars.contains(text.charAt(position))) {
+			returned = returned+text.charAt(position);
+		} else {
+			while (count < text.substring(position, text.length()).length()) {
+				returnedPiece = text.charAt(position + count);
+				if ((!separators.contains(text.charAt(position + count)))&&!importantChars.contains(text.charAt(position+count))) {
+					returned = returned + returnedPiece;
+					count++;
+				} else {
+					count = text.substring(position, text.length()).length();
+				}
+			}
+			count = 0;
+		}
+		return returned;
+	}
 
 }
--- src/test/CoreScannerTest.java
+++ src/test/CoreScannerTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 public class CoreScannerTest {
 
 	@Test
-	public void scanTest() {
+	public void scanTestSource1() {
 		CoreScanner CS = null;
 		try {
 			CS = new CoreScanner("CoreFiles/CoreSource1.core");
@@ -24,11 +24,76 @@ public class CoreScannerTest {
 
 		String[] actual = CS.scan();
 		
+		String[] expected = {"program","int","x",",","y",",","z",";","begin","input","x",",","y",";","z",":","=","x",";","x",":","=","y",";","y",":","=","z",";","output","x",";","output","y",";","end"};
+		for(int i = 0; i<actual.length;i++) {
+		System.out.println(actual[i]);
+		}
+		Assert.assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void scanTestSource2() {
+		CoreScanner CS = null;
+		try {
+			CS = new CoreScanner("CoreFiles/CoreSource2.core");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertNotNull(CS);
+
+		String[] actual = CS.scan();
+		
+		String[] expected = {"program","int","x",",","y",",","z",";","begin","input","x",",","y",";","z",":","=","x",";","x",":","=","y",";","y",":","=","z",";","output","x",";","output","y",";","end"};
+		for(int i = 0; i<actual.length;i++) {
+		System.out.println(actual[i]);
+		}
+		Assert.assertArrayEquals(expected, actual);
+	}
+	
+	
+	@Test
+	public void scanTestSource3() {
+		CoreScanner CS = null;
+		try {
+			CS = new CoreScanner("CoreFiles/CoreSource3.core");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertNotNull(CS);
+
+		String[] actual = CS.scan();
+		
+		String[] expected = {"program","int","x",";","begin","x",":","=","5",";","end"};
+		for(int i = 0; i<actual.length;i++) {
+		System.out.println(actual[i]);
+		}
+		Assert.assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void preprocessTest() {
+		
+		CoreScanner CS = null;
+		try {
+			CS = new CoreScanner("CoreFiles/CoreSource1.core");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertNotNull(CS);
+
+		String[] actual = CS.scan();
+		actual = CS.preprocess(actual);
+		System.out.println(actual.toString());
+		
 		String[] expected = { "PROGRAM", "INT", "ID[x]", "COMMA", "ID[y]", "COMMA", "ID[z]", "SEMICOLON", "BEGIN",
 				"INPUT", "ID[x]", "COMMA", "ID[y]", "SEMICOLON", "ID[z]", "ASSIGN", "ID[x]", "SEMICOLON", "ID[x]",
 				"ASSIGN", "ID[y]", "SEMICOLON", "ID[y]", "ASSIGN", "ID[z]", "SEMICOLON", "OUTPUT", "ID[x]", "SEMICOLON",
 				"OUTPUT", "ID[y]", "SEMICOLON", "END", "EOF" };
-		Assert.assertArrayEquals(expected, actual);
+		
+		Assert.assertArrayEquals(expected,actual);
 	}
 
 }
