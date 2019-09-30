f03276f2d56b74e12c1df74f9e50573c4bcb9de1
curtisisaacson <curtiskisaacson@gmail.com>
2019-09-30 10:01:39 -0400

    Fixed Initial Errors, added basic code

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,15 +1,23 @@
 package code;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
-        CoreScanner CS = new CoreScanner("hey");
+        CoreScanner CS = null;
+		try {
+			CS = new CoreScanner("hey");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         Scanner in = new Scanner(System.in);
 
 
-        String[] tokens = CS.scan(in);
+        String[] tokens = CS.scan();
+        tokens = CS.preprocess(tokens);
 
         for(int i = 0; i< tokens.length; i++){
             System.out.println(tokens[i]);
