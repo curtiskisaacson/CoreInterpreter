4b69dd9a81671c728c8d01b97e43ca222c5b73cd
Curtis Isaacson <curtiskisaacson@gmail.com>
2019-10-04 14:53:14 -0400

    Added code for parse

--- src/code/Program.java
+++ src/code/Program.java
@@ -9,6 +9,33 @@ public class Program {
 	}
 	
 	public void parse() {
+		if(ParseTree.tokens[ParseTree.location] == "PROGRAM") {
+			ParseTree.location++;
+		}else {
+			System.out.println("ERROR: Program keyword expected and not Found");
+			System.exit(0);
+		}
+		
+		this.declSeq= new DeclareSequence();
+		declSeq.parse();
+		//remember parse should automatically update ParseTree.location
+		
+		if(ParseTree.tokens[ParseTree.location] == "BEGIN") {
+			ParseTree.location++;
+		}else {
+			System.out.println("ERROR: Begin keyword expected and not Found");
+			System.exit(0);
+		}
+		
+		stmtSeq = new StatementSequence();
+		stmtSeq.parse();
+		
+		if(ParseTree.tokens[ParseTree.location] == "END") {
+			ParseTree.location++;
+		}else {
+			System.out.println("ERROR: End keyword expected and not Found");
+			System.exit(0);
+		}
 		
 	}
 	
